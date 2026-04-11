47f4c2179426d3c360a994c7115b83733d335a64
Thomas Lhoest <tlhoest@gmail.com>
2026-04-11 14:11:42 +0200

    cleanup fade-in-out logic

--- src/ModelSpawner.ts
+++ src/ModelSpawner.ts
@@ -11,19 +11,7 @@ const MIN_WINDOW_MS     = 1200;
 const MAX_WINDOW_MS     = 4000;
 const MIN_PROB          = 0.15;
 const MAX_PROB          = 0.94;
-const FADE_OUT_MS       = 120;
-
-interface Fading {
-  node:    Node;
-  elapsed: number;
-  total:   number;
-  from:    number;
-  to:      number;
-  onDone?: () => void;
-}
-
 export class ModelSpawner {
-  private fading: Fading[] = [];
   private timer   = 400;
   private seed    = 200;
   private halfW   = 8;
@@ -42,8 +30,6 @@ export class ModelSpawner {
   }
 
   tick(dt: number, _camera: Camera): void {
-    this.tickFading(dt);
-
     // Fill up to MAX_NODES immediately before switching to the timed behaviour.
     if (this.scene.nodes.length < MAX_NODES) {
       const x = (Math.random() * 2 - 1) * this.halfW;
@@ -69,28 +55,11 @@ export class ModelSpawner {
     }
   }
 
-  private tickFading(dt: number): void {
-    for (let i = this.fading.length - 1; i >= 0; i--) {
-      const f = this.fading[i];
-      f.elapsed += dt;
-      const p = Math.min(1, f.elapsed / f.total);
-      f.node.alphaScale = f.from + (f.to - f.from) * p;
-      if (p >= 1) {
-        f.node.alphaScale = f.to;
-        f.onDone?.();
-        this.fading.splice(i, 1);
-      }
-    }
-  }
-
   private spawnNode(worldPos: [number, number, number]): void {
     if (this.scene.nodes.length >= MAX_NODES) {
       const victim = this.scene.nodes[0];
       this.scene.triggerDeleteChaos(victim.physics.pos);
-      this.fading.push({
-        node: victim, elapsed: 0, total: FADE_OUT_MS, from: 1, to: 0,
-        onDone: () => this.scene.removeNode(victim),
-      });
+      this.scene.removeNode(victim);
     }
 
     const model = this.modelPool[this.seed % this.modelPool.length];
